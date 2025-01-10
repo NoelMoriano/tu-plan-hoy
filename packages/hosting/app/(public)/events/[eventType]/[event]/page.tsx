229d63cee63c411cdf90877143331c3af1789b8b
noel <admin@agenciaservitec.com>
2025-01-09 23:52:22 -0500

    Added event page

---
+++ packages/hosting/app/(public)/events/[eventType]/[event]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EventPage() {
+  return (
+    <div className="w-full min-h-[100svh] h-auto">
+      <h1>PAGINA DEL DETALLE DEL EVENTO</h1>
+    </div>
+  );
+}
