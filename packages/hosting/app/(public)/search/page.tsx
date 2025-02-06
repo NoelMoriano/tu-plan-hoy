67d8deddb3a42b31f1ba5dfe8de5a01ee798dcdc
noel <admin@agenciaservitec.com>
2025-02-06 18:36:16 -0500

    Update with responsive

--- packages/hosting/app/(public)/search/page.tsx
+++ packages/hosting/app/(public)/search/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/Button";
 import { WrapperComponent } from "@/components/ui/WrapperComponent";
 import { SearchIcon, WatchIcon } from "lucide-react";
+import { Select } from "@/components/ui/Select";
 
 export default function SearchPage() {
   return (
@@ -18,86 +19,122 @@ export default function SearchPage() {
               <span>Historial</span>
             </div>
           </div>
+          <div className="filter-in-mobile grid lg:hidden grid-cols-2 gap-3 mb-5">
+            <div className="grid place-items-center">
+              <Select
+                label="Categorias"
+                options={[
+                  {
+                    label: "Categorias",
+                    value: "all",
+                  },
+                  {
+                    label: "Salsa y bachata",
+                    value: "sala_and_bachata",
+                  },
+                ]}
+              />
+            </div>
+            <div className="grid place-items-center">
+              <Select
+                label="País"
+                options={[
+                  {
+                    label: "Todos",
+                    value: "all",
+                  },
+                  {
+                    label: "Lima",
+                    value: "lima",
+                  },
+                ]}
+              />
+            </div>
+          </div>
           <div className="content w-full grid grid-cols-1 lg:grid-cols-[1fr,30em] gap-[3em]">
             <div className="left-items">
-              {[1, 2, 3].map((ads, index) => (
-                <div
-                  key={index}
-                  className="bg-quaternary grid grid-cols-[auto,1fr] gap-5 rounded-[10px] p-5 mb-5"
-                >
-                  <div className="fle justify-center items-start">
-                    <Image
-                      src="/images/ads-banner.png"
-                      alt="logo empresa"
-                      width={210}
-                      height={132}
-                      className="object-cover rounded-[5px] md:w-[210px] w-[130px] md:h-[132px] h-[82px]"
-                    />
-                  </div>
-                  <div className="grid gap-2">
-                    <div className="w-full flex gap-2 flex-wrap items-center">
-                      <span className="text-primary font-bold text-[24px] leading-[.8em]">
-                        Inka Team
-                      </span>
-                      <div className="tags flex gap-2">
-                        <div className="tag w-auto h-[27px] rounded-[5px] text-secondary text-[14px] font-semibold grid place-items-center bg-tertiary px-3">
-                          Exclusivas
+              <div className="card-list w-full">
+                {[1, 2, 3].map((ads, index) => (
+                  <div
+                    key={index}
+                    className="bg-quaternary grid gap-3 rounded-[10px] p-3 mb-5"
+                  >
+                    <div className="grid grid-cols-1 xs:grid-cols-[auto,1fr] gap-5">
+                      <div className="grid">
+                        <Image
+                          src="/images/ads-banner.png"
+                          alt="logo empresa"
+                          width={210}
+                          height={132}
+                          className="object-cover rounded-[5px] w-full h-auto xs:w-[190px] xs:h-[122px]"
+                        />
+                      </div>
+                      <div className="grid gap-2">
+                        <div className="w-full flex gap-2 flex-wrap items-center">
+                          <span className="text-primary font-bold text-[20px] md:text-[24px] leading-[.8em]">
+                            Inka Team
+                          </span>
+                          <div className="tags flex gap-2">
+                            <div className="tag w-auto h-[24px] md:h-[27px] rounded-[5px] text-secondary text-[10px] md:text-[12px] text-center leading-[1em] font-semibold grid place-items-center bg-tertiary px-3">
+                              Exclusivas
+                            </div>
+                            <div className="tag w-auto h-[24px] md:h-[27px] rounded-[5px] text-secondary text-[10px] md:text-[12px] text-center leading-[1em] font-semibold grid place-items-center bg-tertiary px-3">
+                              Música Electrónica
+                            </div>
+                          </div>
                         </div>
-                        <div className="tag w-auto h-[27px] rounded-[5px] text-secondary text-[14px] font-semibold grid place-items-center bg-tertiary px-3">
-                          Música Electrónica
+                        <div className="w-full flex justify-between flex-wrap gap-2">
+                          <div className="hours">
+                            <div className="item flex items-center gap-2">
+                              <div>
+                                <span className="text-secondary font-medium text-[12px] md:text-[14px]">
+                                  Inicio:
+                                </span>{" "}
+                                <span className="text-primary font-semibold text-[13px] md:text-[15px]">
+                                  20/Dic/2024
+                                </span>
+                              </div>
+                              <div>
+                                <span className="text-secondary font-medium text-[12px] md:text-[14px]">
+                                  a las:
+                                </span>{" "}
+                                <span className="text-primary font-semibold text-[13px] md:text-[15px]">
+                                  12:00 AM
+                                </span>
+                              </div>
+                            </div>
+                            <div className="item flex items-center gap-2">
+                              <div>
+                                <span className="text-secondary font-medium text-[12px] md:text-[14px]">
+                                  Final:
+                                </span>{" "}
+                                <span className="text-primary font-semibold text-[13px] md:text-[15px]">
+                                  20/Dic/2024
+                                </span>
+                              </div>
+                              <div>
+                                <span className="text-secondary font-medium text-[12px] md:text-[14px]">
+                                  a las:
+                                </span>{" "}
+                                <span className="text-primary font-semibold text-[13px] md:text-[15px]">
+                                  06:00 AM
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="btns flex items-center gap-2">
+                            <Button className="h-[37px] grid place-items-center py-2 px-4">
+                              Ver anuncio
+                            </Button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                    <div className="w-full flex justify-between flex-wrap gap-2">
-                      <div className="hours">
-                        <div className="item flex items-center gap-2">
-                          <div>
-                            <span className="text-secondary font-medium text-[14px]">
-                              Inicio:
-                            </span>{" "}
-                            <span className="text-primary font-semibold text-[15px]">
-                              20/Dic/2024
-                            </span>
-                          </div>
-                          <div>
-                            <span className="text-secondary font-medium text-[14px]">
-                              a las:
-                            </span>{" "}
-                            <span className="text-primary font-semibold text-[15px]">
-                              12:00 AM
-                            </span>
-                          </div>
-                        </div>
-                        <div className="item flex items-center gap-2">
-                          <div>
-                            <span className="text-secondary font-medium text-[14px]">
-                              Final:
-                            </span>{" "}
-                            <span className="text-primary font-semibold text-[15px]">
-                              20/Dic/2024
-                            </span>
-                          </div>
-                          <div>
-                            <span className="text-secondary font-medium text-[14px]">
-                              a las:
-                            </span>{" "}
-                            <span className="text-primary font-semibold text-[15px]">
-                              06:00 AM
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="btns flex items-center gap-2">
-                        <Button className="h-[37px] grid place-items-center py-2 px-4">
-                          Ver más
-                        </Button>
-                      </div>
-                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-            <div className="right-items">
+            <div className="right-items hidden lg:block">
               <div className="categories mb-[2em]">
                 <div className="category-title text-[16px] text-primary font-bold mb-[1em]">
                   Categoría
--- packages/hosting/components/ui/Select.tsx
+++ packages/hosting/components/ui/Select.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type Option = {
-  label: string;
+  label: string | React.ReactNode;
   value: string;
 };
 
@@ -9,11 +9,12 @@ interface Props {
   label?: string;
   placeholder?: string;
   options: Option[];
+  multiple?: boolean;
 }
 
-export const Select = ({ label, options = [] }: Props) => {
+export const Select = ({ label, options = [], multiple }: Props) => {
   return (
-    <div>
+    <div className="w-full">
       {label && (
         <div>
           <label
@@ -25,7 +26,10 @@ export const Select = ({ label, options = [] }: Props) => {
         </div>
       )}
       <div className="w-full h-[47px] p-1 px-2 pr-3 bg-white border-tertiary border-[1px] rounded-[5px] outline-none m-0 overflow-hidden">
-        <select className="w-full h-full p-1 text-secondary text-[14px] outline-none m-0">
+        <select
+          className="w-full h-full p-1 text-secondary text-[14px] outline-none m-0"
+          multiple={multiple}
+        >
           {options.map((option, index) => (
             <option key={option?.value || index} value={option.value}>
               {option.label}
